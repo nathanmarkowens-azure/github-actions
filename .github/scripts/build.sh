c1ca5a982f5779cefba0ed9f5a9c2a8b3e919d88
venminder-nathanowens <nathan.owens@venminder.com>
2024-04-01 08:58:10 -0400

    update image tag to first 6 characters, add terraform destroy action, add workflow dispatch variable for terraform destroy

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Get first 6 characters of commit sha for image tag
 COMMIT_SHA=${GITHUB_SHA:-$(git rev-parse HEAD)}
-IMAGE_TAG=$COMMIT_SHA
+IMAGE_TAG=${COMMIT_SHA:0:6}
 
+# Build web image
 docker build . \
   --tag web:$IMAGE_TAG \
   --file Dockerfile \
